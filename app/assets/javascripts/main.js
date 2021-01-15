8f9e13d8a797298377e9f0985b9362238f2002a4
chetan Batra <chetanbatra25@gmail.com>
2021-01-15 19:17:42 +0530

    Add the content on homepage

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,102 @@
+(function($){
+	"use strict";
+
+	// Header Sticky
+	$(window).on('scroll',function() {
+		if ($(this).scrollTop() > 120){  
+			$('.navbar-area').addClass("is-sticky");
+		}
+		else{
+			$('.navbar-area').removeClass("is-sticky");
+		}
+	});
+
+	// Others Option Responsive JS
+	$(".others-option-for-responsive .dot-menu").on("click", function(){
+		$(".others-option-for-responsive .container .container").toggleClass("active");
+	});
+
+	// Button Hover JS
+    $(function() {
+		$('.default-btn')
+		.on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+			relX = e.pageX - parentOffset.left,
+			relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+		})
+		.on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+			relX = e.pageX - parentOffset.left,
+			relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+		});
+	});
+
+
+	
+	$('.project-start-area').mousemove(function(e){
+		var wx = $(window).width();
+		var wy = $(window).height();
+		var x = e.pageX - this.offsetLeft;
+		var y = e.pageY - this.offsetTop;
+		var newx = x - wx/2;
+		var newy = y - wy/2;
+		$('.project-start-image img').each(function(){
+			var speed = $(this).attr('data-speed');
+			if($(this).attr('data-revert')) speed *= -1;
+			TweenMax.to($(this), 1, {x: (1 - newx*speed), y: (1 - newy*speed)});
+		});
+	});
+	
+	
+	// Testimonials Slides
+	
+	// Tabs
+	(function ($) {
+		$('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
+		$('.tab ul.tabs li a').on('click', function (g) {
+			var tab = $(this).closest('.tab'), 
+			index = $(this).closest('li').index();
+			tab.find('ul.tabs > li').removeClass('current');
+			$(this).closest('li').addClass('current');
+			tab.find('.tab-content').find('div.tabs-item').not('div.tabs-item:eq(' + index + ')').slideUp();
+			tab.find('.tab-content').find('div.tabs-item:eq(' + index + ')').slideDown();
+			g.preventDefault();
+		});
+	})(jQuery);
+
+	// FAQ Accordion
+	$(function() {
+		$('.accordion').find('.accordion-title').on('click', function(){
+			// Adds Active Class
+			$(this).toggleClass('active');
+			// Expand or Collapse This Panel
+			$(this).next().slideToggle('fast');
+			// Hide The Other Panels
+			$('.accordion-content').not($(this).next()).slideUp('fast');
+			// Removes Active Class From Other Titles
+			$('.accordion-title').not($(this)).removeClass('active');		
+		});
+	});
+
+	// Go to Top
+	$(function(){
+		// Scroll Event
+		$(window).on('scroll', function(){
+			var scrolled = $(window).scrollTop();
+			if (scrolled > 600) $('.go-top').addClass('active');
+			if (scrolled < 600) $('.go-top').removeClass('active');
+		});  
+		// Click Event
+		$('.go-top').on('click', function() {
+			$("html, body").animate({ scrollTop: "0" },  500);
+		});
+	});
+
+	// Preloader
+    $(window).on('load', function () {
+        $('.preloader').fadeOut();
+    });
+
+}(jQuery));
