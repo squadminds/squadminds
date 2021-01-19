2cf687a5176cbf2ecd945ffe3f7d249fd5485fac
chetan Batra <chetanbatra25@gmail.com>
2021-01-19 18:57:06 +0530

    Fix all javascript, images tag, stylesheets

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,14 @@
 class HomeController < ApplicationController
+    def coming_soon
+    end
+
+    def contact
+    end
+
+    def about_us
+    end
+
+    def services
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root 'home#coming_soon'
+  get 'contact', to: "home#contact"
+  get 'about_us', to: "home#about_us"
+  get 'services', to: "home#services"
+ # get "/unsubscribe/random_email/:id", to: "unsubscribe_emails#random_email", as: :random_entry_unsubscribe
+
 end
