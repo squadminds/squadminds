710a5bc13a78af47f49e2fc14a43ec71f9177c5c
chetan Batra <chetanbatra25@gmail.com>
2021-01-20 16:58:10 +0530

    feat: changes for new website layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,2 @@
 class HomeController < ApplicationController
-    def coming_soon
-    end
-
-    def contact
-    end
-
-    def about_us
-    end
-
-    def services
-    end
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'home#coming_soon'
-  get 'contact', to: "home#contact"
-  get 'about_us', to: "home#about_us"
+  root 'home#index'
+  get 'contact-us', to: "home#contact_us"
+  get 'about-us', to: "home#about_us"
   get 'services', to: "home#services"
  # get "/unsubscribe/random_email/:id", to: "unsubscribe_emails#random_email", as: :random_entry_unsubscribe
 
