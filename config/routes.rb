078cd4db17defae870f94eba67d5d8513feec142
chetan Batra <chetanbatra25@gmail.com>
2021-01-22 17:33:12 +0530

    Remove padding, add testimonials and projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'contact-us', to: "home#contact_us"
   get 'about-us', to: "home#about_us"
   get 'services', to: "home#services"
+  get 'testimonials', to: "home#testimonials"
  # get "/unsubscribe/random_email/:id", to: "unsubscribe_emails#random_email", as: :random_entry_unsubscribe
 
 end
